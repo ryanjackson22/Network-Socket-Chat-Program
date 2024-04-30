39955ddebadb9615af440497d5ef960b55bb3641
ryanjackson <ryan.jackson@mymail.champlain.edu>
2024-04-30 19:36:05 -0400

    created 'accept_connection()' & passed it into 'reading_thread'; init()

--- Network-Socket-Chat-Program/src/main/server.py
+++ Network-Socket-Chat-Program/src/main/server.py
@@ -16,7 +16,15 @@ class Server:
         self.writing_socket = create_socket(host, port + 1)
 
         # These sockets will be passed to their own individual threads, which should operate as follows:
-        reading_thread = threading.Thread(target=None, args=())
+        reading_thread = threading.Thread(target=self.accept_connections(), args=())
+
+        writing_thread = threading.Thread(target=None, args=())
+        # Todo sit in a loop waiting for START messages from client receiving threads.
+
+        # Todo When it receives such a message,
+        #  it should add this socket to the above global variable
+
+    def accept_connections(self):
         # TODO This should sit in a loop accepting connections from client sending threads.
 
         # TODO When a client connection is accepted,
@@ -25,12 +33,7 @@ class Server:
         # TODO Each new thread should accept messages from the accept socket it has been given,
         #  determine what sort of messages they are (BROADCAST? PRIVATE? EXIT?)
         #  choose the appropriate action.
-        writing_thread = threading.Thread(target=None, args=())
-        # Todo sit in a loop waiting for START messages from client receiving threads.
-
-        # Todo When it receives such a message,
-        #  it should add this socket to the above global variable
-
+        pass
 
 def create_socket(host: str, port: int) -> socket:
     new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
