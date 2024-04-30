8cb3e642cd4d5659056b138b6a6cc98d11edb063
ryanjackson <ryan.jackson@mymail.champlain.edu>
2024-04-30 19:14:56 -0400

    created ability to choose username

--- Network-Socket-Chat-Program/src/main/client.py
+++ Network-Socket-Chat-Program/src/main/client.py
@@ -4,10 +4,21 @@ import socket
 import threading
 
 
+def is_username_invalid(username: str) -> bool:
+    if not username:
+        return True
+
+    return False
+
+
 class Client:
     """docstring for Client"""
     def __init__(self):
         # Todo The client should first start by letting the user pick a screen name for their client,
+        self.username = ""
+        while is_username_invalid(self.username):
+            self.username = input("Enter a Server Username: ")
+
         #  apply restrictions as needed
 
         self.sending_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # handles sending messages
@@ -27,18 +38,18 @@ class Client:
         #  + the screen name of the sender when printed,
         #  + indication as to whether the message was private or not.
 
-        # recv socket
-        self.recv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.recv_socket.listen(20)
-        # send socket
-        self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.send_socket.connect(("localhost", 10000))
-
-        send_thread = threading.Thread(target=self.send_message(), args=())
-        send_thread.start()
-        server_connection, server_address = self.recv_socket.accept()
-        recv_thread = threading.Thread(target=self.recv_message(server_connection), args=())
-        recv_thread.start()
+        # # recv socket
+        # self.recv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # self.recv_socket.listen(20)
+        # # send socket
+        # self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # self.send_socket.connect(("localhost", 10000))
+        #
+        # send_thread = threading.Thread(target=self.send_message(), args=())
+        # send_thread.start()
+        # server_connection, server_address = self.recv_socket.accept()
+        # recv_thread = threading.Thread(target=self.recv_message(server_connection), args=())
+        # recv_thread.start()
 
     def recv_message(self, server_connection):
         while True:
