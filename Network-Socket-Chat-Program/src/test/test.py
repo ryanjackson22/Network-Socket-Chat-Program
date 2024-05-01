d4d1fe085f93a5014723eb129ff5b1e04a306520
ryanjackson <ryan.jackson@mymail.champlain.edu>
2024-04-30 20:40:19 -0400

    copied logic from is_exit() to complete is_broadcast() & wrote proper tests

--- Network-Socket-Chat-Program/src/test/test.py
+++ Network-Socket-Chat-Program/src/test/test.py
@@ -15,6 +15,16 @@ class MyTestCase(unittest.TestCase):
         actual = src.main.server.is_exit("EXIT asdfsaexitasdf")
         self.assertEqual(expected, actual)
 
+    def test_is_broadcast_false(self):
+        expected = False
+        actual = src.main.server.is_broadcast("allsaexitasdf")
+        self.assertEqual(expected, actual)
+
+    def test_is_broadcast_true(self):
+        expected = True
+        actual = src.main.server.is_broadcast("ALL asdfsaexitasdf")
+        self.assertEqual(expected, actual)
+
 
 if __name__ == '__main__':
     unittest.main()
