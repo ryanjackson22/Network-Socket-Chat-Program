6d93ad6a53f0a5e83ae2418e5c7d8225973af178
ryanjackson <ryan.jackson@mymail.champlain.edu>
2024-04-30 21:28:26 -0400

    removed extra comments from 'send_start_message()'

--- Network-Socket-Chat-Program/src/main/client.py
+++ Network-Socket-Chat-Program/src/main/client.py
@@ -39,10 +39,7 @@ class Client:
         #  + the screen name of the sender when printed,
         #  + indication as to whether the message was private or not.
     def send_start_message(self):
-
-        # todo 1. It should start by sending a START message to the server
-        #  (this should allow your server to collect the screen name
-        #  and socket data it needs to send messages to this client).
+        self.sending_socket.sendall(f"{self.username} START CONNECTION_DETAILS".encode('ascii'))
 
     def listen_user_input(self, sending_socket):
         pass
